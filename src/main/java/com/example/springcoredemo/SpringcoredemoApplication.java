5a884c1bc9d107f0438094cbba42bdc434f3bb65
karanara <karanara@mail.uc.edu>
2023-11-20 10:24:33 -0500

    component scanning

--- src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,8 @@ package com.example.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//scans components outside of the package with this syntax
+//@SpringBootApplication(scanBasePackages= {"com.example.springcoredemo","util"})
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
